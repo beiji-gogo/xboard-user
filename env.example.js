9616b095eaa2ceb1ccc1e93030e3061af7c26763
beiji-gogo <Mlim4450@gmail.com>
2025-03-30 16:34:57 +0800

    Update env.example.js

--- env.example.js
+++ env.example.js
@@ -1,17 +1,17 @@
 // API地址
-window.routerBase = 'http://127.0.0.1:8000/'
+window.routerBase = 'https://panel.44s4.vip/'
 window.settings = {
   // 站点名称
-  title: 'Xboard',
+  title: '北极云',
   // 站点描述
-  description: 'Xboard',
+  description: '北极云',
   assets_path: '/assets',
   // 主题色
   theme: {
-    color: 'default', //可选default、blue、black、、darkblue
+    color: 'blue', //可选default、blue、black、、darkblue
   },
   // 版本号
-  version: '0.1.1-dev',
+  version: '0.1.1',
   // 登陆背景
   background_url: '',
   // 站点LOGO
